d13a387b718d04be4fab718a7e4e24c12ecdd76a
skozlovtsev <svkozlovcev@gmail.com>
2024-03-12 13:49:52 +0300

    some changes

--- Server/server.go
+++ Server/server.go
@@ -12,6 +12,12 @@ import (
 	gmc "github.com/bradfitz/gomemcache/memcache"
 )
 
+type Student struct {
+	Id          int
+	Ident       string
+	NameSurname string
+}
+
 var (
 	MemeClient *gmc.Client
 	DB         *gorm.DB
@@ -28,16 +34,24 @@ func main() {
 func init() {
 	MemeClient = gmc.New("/server")
 
-	dsm := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/UTC", os.Getenv("HOST_NAME"), os.Getenv("USER_NAME"), os.Getenv("PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("PORT"))
+	dsm := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/UTC",
+		os.Getenv("HOST_NAME"),
+		os.Getenv("USER_NAME"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("PORT"))
+
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsm), &gorm.Config{})
+
+	DB.AutoMigrate(Student{})
 
 	if err != nil {
 		panic(err.Error())
 	}
 }
 
-func CheckCase(key string) string {
+func CheckMeme(key string) string {
 	item, err := MemeClient.Get(key)
 
 	if err != nil {
@@ -49,6 +63,27 @@ func CheckCase(key string) string {
 	}
 
 	MemeClient.Touch(key, int32(time.Hour*24/time.Second))
+
+	return key
+}
+
+func AddMeme(student Student) error {
+	err := MemeClient.Add(&gmc.Item{Key: student.Ident, Value: []byte(student.NameSurname), Expiration: int32(time.Hour * 24 / time.Second), Flags: 0})
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func GetFromDB(key string) string {
+	student := Student{}
+	r := DB.Where("ident = ?", key).First(&student)
+
+	if r.Error != nil {
+		return ""
+	}
 
 	return key
 }

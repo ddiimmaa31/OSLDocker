b2a0644589eb10ad8284c5067cc58aa846f02fb5
M0HTeP <artem-ryazanov1991@yandex.ru>
2024-03-13 17:11:13 +0300

    Co-authored-by: Sergey <skozlovtsev@users.noreply.github.com>

--- Server/server.go
+++ Server/server.go
@@ -18,6 +18,11 @@ type Student struct {
 	NameSurname string `gorm:"unique"`
 }
 
+const (
+	home     = " <a href=\"/\">back</a>"
+	register = " <a href=\"/new\">try again</a>"
+)
+
 var (
 	MemeClient *gmc.Client
 	DB         *gorm.DB
@@ -27,22 +32,26 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		ident := r.FormValue("ident")
 		if ident == "" {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 			w.Write([]byte(`<form action="/" method="GET">
 								<input id="ident" type="text" name="ident" />
-								<input type="submit" value="Save" />
+								<input type="submit" value="Check" />
 		  					</form>`))
 			return
 		}
 		if meme := CheckMeme(ident); meme != "" {
-			w.Write([]byte(meme))
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.Write([]byte("<h>" + meme + "</h>" + home))
 			return
 		}
 		s := GetFromDB(ident)
 		if s.Ident != "" {
-			w.Write([]byte(s.NameSurname))
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.Write([]byte("<h>" + s.NameSurname + "</h>" + home))
 			AddMeme(s)
 			return
 		}
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.Write([]byte(`<form action="/new" method="GET">
 							<input id="ident" type="text" name="ident" />
 							<input id="nameSurname" type="text" name="nameSurname" />
@@ -54,6 +63,7 @@ func main() {
 		ident := r.FormValue("ident")
 		nameSurname := r.FormValue("nameSurname")
 		if ident == "" || nameSurname == "" {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
 			w.Write([]byte(`<form action="/new" method="GET">
                                 <input id="ident" type="text" name="ident" />
                                 <input id="nameSurname" type="text" name="nameSurname" />
@@ -61,27 +71,27 @@ func main() {
                             </form>`))
 			return
 		}
-		if WriteToDB(Student{
-			Ident:       ident,
-			NameSurname: nameSurname,
-		}) != nil {
-			w.Write([]byte("Wrong Iednt or NameSurname"))
+		if err := WriteToDB(Student{Ident: ident, NameSurname: nameSurname}); err != nil {
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.Write([]byte("<h>Wrong Iednt or NameSurname</h>" + register))
 		}
-		w.Write([]byte("Student was successfully saved"))
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte("<h>Student was successfully saved</h>" + home))
 	})
 
 	fmt.Println(http.ListenAndServe(os.Getenv("GOSERVER_PORT"), nil))
 }
 
 func init() {
+	time.Sleep(10 * time.Second)
 	MemeClient = gmc.New(os.Getenv("MEMC_PATH"))
 
-	dsm := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/UTC",
+	dsm := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Europe/Moscow",
 		os.Getenv("HOST_NAME"),
 		os.Getenv("USER_NAME"),
 		os.Getenv("PASSWORD"),
 		os.Getenv("DB_NAME"),
-		os.Getenv("PORT"))
+		os.Getenv("POST_PORT"))
 
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsm), &gorm.Config{})
@@ -106,7 +116,7 @@ func CheckMeme(key string) string {
 
 	MemeClient.Touch(key, int32(time.Hour*24/time.Second))
 
-	return key
+	return string(item.Value)
 }
 
 func AddMeme(student Student) error {
@@ -131,7 +141,7 @@ func GetFromDB(key string) Student {
 }
 
 func WriteToDB(student Student) error {
-	r := DB.Create(student)
+	r := DB.Create(&student)
 
 	if r.Error != nil {
 		return r.Error
